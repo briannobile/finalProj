d3ee7f2860ff5a5b12b3fc2a89340c2711289dce
briannobile <brian.nobile.contractor@macmillan.com>
2016-12-21 21:09:32 -0500

    first draft working version

--- js/main.js
+++ js/main.js
@@ -1,17 +1,57 @@
 // first, retrieve all HTML elements with class = js-search
 // and store into variable as jQuery object
 const search = $('.js-search');
-const buttonSearch = $('.js-reset');
 
 // set up a keypress event handler on the jQuery object that we selected
 search.keypress(onKeyPressed);
-buttonSearch.click(onKeyPressed);
+
+const movieSearchButton = $('.js-search-btn');
+movieSearchButton.click(onSearchPress);
+
+	const urlToRequest = `http://www.omdbapi.com/?t=Jaws&tomatoes=true&y=&plot=short&r=json`;
+	console.log(urlToRequest)
+
+	const data = $.get( urlToRequest );
+	
+	data.then(onDataBack);
+
+
+
+function onSearchPress(e) {
+
+	const currentElement = $('.js-search');
+
+	const queryString = currentElement.val();
+
+	
+	console.log('enter key pressed', queryString)
+
+	//skip the updating of the label for now...
+
+
+	const container = $('.js-container');
+	container.html('');
+
+	const urlToRequest = `http://www.omdbapi.com/?t=${queryString}&tomatoes=true&y=&plot=short&r=json`;
+	console.log(urlToRequest)
+
+	const data = $.get( urlToRequest );
+	
+	data.then(onDataBack);
+}
+
 
 
 //??can't get search buttont to work
 
-//1. set up search button
+//1. set up search button...
+	//how to tie it to the onKeyPressed function???
+
 //2. wrap images in clear fix...
+	//can't seem to get both images to appear in same div
+	//is container too narrow?
+
+//3. set up some kind of scoring styling...
 
 
 //clear search field on button press
@@ -100,6 +140,12 @@ function onDataBack(response) {
 
 //posts RT rating
 	tomatoRating = response.tomatoMeter;
+	if (tomatoRating === "N/A") {
+		movieData.append("pick another movie")
+		$('input[type="text"]').val('');
+		 return
+	}
+
 	// const tomatoMeter = parseInt(response.tomatoMeter, 10);
 	console.log(tomatoRating);
 
@@ -116,21 +162,21 @@ function onDataBack(response) {
 	// const tomPlot = `<div>Plot: ${tomatoPlot}</div>`;
 
 //rules of the game
-	const tomRules = `<div class="js-label">click rotten image if score is less than 50, tomato if above</div>`;
+	const tomRules = `<div class="js-label label-style">Click rotten if score is less than 50, tomato if above</div>`;
 
 
 //posts rotten image
 	const rottenImg = `<img src="images/rotten.png">`;
 	// console.log("rottenImg");
 	// const tomRotten = `${rottenImg}`;
-	const tomRotten = `<div class="js-rotten-button">${rottenImg}</div>`;
+	const tomRotten = `<div class="js-rotten-button button-choices">${rottenImg}</div>`;
 	// console.log("tomRotten");
 
 //posts fresh image
 	const freshImg = `<img src="images/fresh.png">`;
 	// console.log("freshImg");
 	// const tomFresh = `${freshImg}`;
-	const tomFresh = `<div class="js-fresh-button">${freshImg}</div>`;
+	const tomFresh = `<div class="js-fresh-button button-choices">${freshImg}</div>`;
 	// console.log("tomFresh");
 
 	movieData.append(movieHtml, tomRules, tomRotten, tomFresh);
@@ -150,11 +196,11 @@ $('body').on('click', '.js-rotten-button', imageEnter);
 function imageEnter(e){
 	
 	if (tomatoRating < 50){
-		$('.js-label').text("correct!");
-	console.log('correct');
+		$('.js-label').text("Correct! Nice Job!");
+		console.log('correct');
 	} else {
-		$('.js-label').text("wrong!");
-	console.log('wrong');
+		$('.js-label').text("Wrong! Try another movie");
+		console.log('wrong');
 	}
 
 };
@@ -166,30 +212,15 @@ $('body').on('click', '.js-fresh-button', imageEnterFresh);
 function imageEnterFresh(e){
 	
 	if (tomatoRating >= 50){
-		$('.js-label').text("correct");
-	console.log('correct');
+		$('.js-label').text("Correct! Good guess");
+		console.log('correct');
 	} else {
-		$('.js-label').text("wrong");
-	console.log('wrong');
+		$('.js-label').text("Incorrect, enter another movie");
+		console.log('wrong');
 	}
 };
 
 
-///how to set up a functionthat grabs the score
-
-//then determines what 
-
-///how to get the actual score
-
-// function tomatoRatingScore(response) {
-// 	const tomatoRating = response.tomatoMeter;
-// 	console.log(tomatoRating);
-// }
-// if (tomatoRating >= 50) {
-// 	console.log('movie is fresh');
-//  } else {
-//  	console.log('movie is rotten');
-//  }
 
 
 
@@ -197,108 +228,3 @@ function imageEnterFresh(e){
 
 
 
-
-//can't get random number to generate as a score an be placed in html
-
-
-//put icons under the movie photo
-	// const rottenImg = <img src="http://www.billdamon.com/images/rottentomatoes/rotten.png">;
-	// console.log(trottenImg);
-	// const tomPlot = `<img src="http://www.billdamon.com/images/rottentomatoes/rotten.png">`;
-
-
-
-
-		  
-// 		 </p>
-// 	`;
-
-//  	const tomatoMeter = parseInt(response.tomatoMeter, 10);
-
-//  	console.log(tomatoMeter, typeof tomatoMeter);
-
-
-// 	generateGameScores(tomatoMeter);
-
-// 	movieData.append(movieHtml);
-// }
-
-
-/*
-// this function runs when the user initiates a keystroke on the
-// jQuery object called `search`
-function onKeyPressed(e) {
-	// check the `which` property of our `e` object - the event object
-	// if `e.which` is 13 then it is an enter key press
-	if (e.which === 13) {
-		// in here we initialize the logic for our giphy search engine program
-
-		const currentElement = $(this);
-		// ^^^ currentElement stores a jQuery object out of the
-		// specific element that we have initiated the event on
-
-		// now, grab the value of the current input field
-		// so that we can...
-		// use it in our GET request (ie: this queryString is what 
-		// we will search for against the Giphy API)
-		const queryString = currentElement.val();
-		console.log('enter key pressed', queryString)
-
-
-		// update the label, yo
-		const label = $('.js-label');
-		label.text(queryString);
-
-		// the container stores the search results
-		// however we are not sure if we are doing a search for
-		// the first time or ten time...TBH, IDGAF
-		// we just want to clear it every time a search occurs
-		const container = $('.js-container');
-		container.html('');
-
-		// assemble the correct api endpoint that we need to make a request to the giphy api
-		// NOTICE the querystring in the urlToRequest wrapped in the ${}
-		const urlToRequest = `http://api.giphy.com/v1/gifs/search?q=${queryString}&api_key=dc6zaTOxFJmzC`;
-		console.log(urlToRequest)
-
-		// we are now storing into the variable `data` an object
-		// that represents our network request to the Giphy API
-		const data = $.get( urlToRequest );
-
-		// WHEN the network request comes back, THEN the `data` object
-		// will run our `onDataBack` function to handle the successful response
-		data.then(onDataBack);
-	}
-}
-
-// this function will run WHEN the network request returns **successfully**
-function onDataBack(response) {
-	console.log('data is back!')
-	console.log(response);
-
-	// get the container in jQuery
-	const container = $('.js-container');
-
-	// `response.data` is...
-	// an array. AN array is a list of things that are similar in nature
-	// the `forEach` is a function that will run on EACH item in our array
-	response.data.forEach(eachDataItem);
-
-
-	// this is the function that we are calling from our `forEach`
-	function eachDataItem(currentItem) {
-		// `currentItem` = response.data[0] or response.data[1]
-		// for each `currentItem`, grab the downsized image url
-		const url = currentItem.images.downsized_large.url;
-		console.log(url)
-
-		// create a string that is an img tag
-		const imgTag = `<img class="search-img" src="${url}">`;
-		console.log(imgTag)
-		
-		// insert it into the container for esults
-		container.append(imgTag)
-	}
-}
-
-*/
